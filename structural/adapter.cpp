d349b79618ba087eeb631bb39b501026f8c7d9b2
Picsart Academy <picsartacademy@MacBook-Pro2.local>
2026-03-17 11:48:52 +0400

    newly added structural dir and newly added adapter.cpp file inside it

---
+++ structural/adapter.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <cmath>
+
+class RoundPeg {
+    float radius;
+public:
+    RoundPeg(float radius) : radius(radius) {}
+    RoundPeg() = default;
+    virtual float getRadius() const {
+        return radius;
+    }
+};
+
+class RoundHole {
+    float radius;
+public:
+    RoundHole(float radius) : radius(radius) {}
+    RoundHole() = default;
+    float getRadius() const {
+        return radius;
+    }
+
+    bool fits(RoundPeg& peg) {
+        return radius >= peg.getRadius();
+    }
+};
+
+
+class SquarePeg {
+    float width;
+public:
+    SquarePeg(float width) : width(width) {}
+    SquarePeg() = default;
+    float getWidth() const {
+        return width;
+    }
+};
+
+
+class SqurePegAdapter : public RoundPeg {
+    const SquarePeg& peg;
+
+public:
+    SqurePegAdapter(const SquarePeg& peg) : peg(peg) {}
+
+    float getRadius() const override {
+        return peg.getWidth() * (sqrt(2) / 2);
+    }
+};
+
+int main() {
+    RoundHole hole(10);
+    RoundPeg peg(5);
+    SquarePeg peg2(9);
+
+    SqurePegAdapter AdaptedSquarePed(peg2);
+    std::cout << hole.fits(AdaptedSquarePed) << std::endl;
+}
